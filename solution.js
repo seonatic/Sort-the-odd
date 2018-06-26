5a63e14807a9cde95ae01a0c5a1de27c119b64a0
CESAR NAVARRO <34008442+seonatic@users.noreply.github.com>
2018-06-26 20:07:08 +0200

    comments

--- solution.js
+++ solution.js
@@ -1,8 +1,8 @@
-function sortArray(array) {
-  // Return a sorted array.
+
   
   function sortArray(array) {
-   const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
-   return array.map((x) => x % 2 ? odd.shift() : x);
-  }
+   const odd = array.filter((x) => x % 2).sort((a,b) => a - b);  // sorted array of odd numbers [1,3,5]
+   return array.map((x) => x % 2 ? odd.shift() : x); //new array checks if the original array element is odd 
+                                                     //and if its true shift elements form the sorted odd array. 
+  
 }
