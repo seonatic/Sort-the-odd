39578f9333dbc63ebfe05947c8d384e49fc67b6b
CESAR NAVARRO <34008442+seonatic@users.noreply.github.com>
2018-06-26 13:54:26 +0200

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,8 @@
+function sortArray(array) {
+  // Return a sorted array.
+  
+  function sortArray(array) {
+   const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+   return array.map((x) => x % 2 ? odd.shift() : x);
+  }
+}
